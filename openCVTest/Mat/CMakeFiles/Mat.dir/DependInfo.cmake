7143651c0c4c697080f3298425d686b2392f742b
Fredrikkortet <fredrikkortetjarvi@protonmail.com>
2020-12-08 21:09:45 +0000

    Add Mat and practice folder

--- openCVTest/Mat/CMakeFiles/Mat.dir/DependInfo.cmake
+++ openCVTest/Mat/CMakeFiles/Mat.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/tipparn/Documents/Programmering/Cpp/openCVTest/DisplayImage/DisplayImage.cpp" "/home/tipparn/Documents/Programmering/Cpp/openCVTest/DisplayImage/CMakeFiles/DisplayImage.dir/DisplayImage.cpp.o"
+  "/home/tipparn/Documents/Programmering/Cpp/openCVTest/Mat/Mat.cpp" "/home/tipparn/Documents/Programmering/Cpp/openCVTest/Mat/CMakeFiles/Mat.dir/Mat.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
